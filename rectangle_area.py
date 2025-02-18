69b5ddb63a4831d1aaf92044cb7758b5d87c1d7f
aglmhn <agilmahendra7@gmail.com>
2025-02-18 12:42:12 +0700

    feature/perimeter

--- rectangle_area.py
+++ rectangle_area.py
@@ -5,4 +5,6 @@ def calculate_area(length, width):
 length = 5
 width = 10
 area = calculate_area(length, width)
+perimeter = calculate_perimeter(length, width)
+
 print(f"The area of the rectangle is: {area}")
